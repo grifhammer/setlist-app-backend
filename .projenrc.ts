88cd610a8a2ed5229bc2e02da8e3ea169cccb022
Griffin Hammer <grifhammer@gmail.com>
2022-03-04 22:03:32 -0800

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,13 @@
-const { awscdk } = require('projen');
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'serverless-backend',
-  deps: [] /* Runtime dependencies of this module. */,
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  name: "serverless-backend",
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-project.tsconfig.tsconfig.forceConsistentCasingInFileNames = true;
-project.tsconfig.tsconfig.project.synth();
+project.synth();
