444139e119c8beed959df67cdbf8bcd42236b8d9
Griffin Hammer <grifhammer@gmail.com>
2022-03-04 22:23:54 -0800

    Initial commit of code changes

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,18 @@
 import { awscdk } from "projen";
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  name: "serverless-backend",
-  projenrcTs: true,
+	cdkVersion: "2.1.0",
+	defaultReleaseBranch: "main",
+	name: "setlist-app-serverless-backend",
+	projenrcTs: true,
+	tsconfig: {
+		compilerOptions: {
+			forceConsistentCasingInFileNames: true,
+		},
+	},
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+	// deps: [],                /* Runtime dependencies of this module. */
+	// description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+	// devDeps: [],             /* Build dependencies for this module. */
+	// packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
